4d1ddf4e0b57d23d7b5a653b4528934565812b8d
LavenderIris <pchung@gmail.com>
2017-11-02 12:23:50 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Dojo Mail';
+  peoples = [{email: 'bill@gates.com', importance: true, subject: 'New Windows', content: 'Windows will launch in year 2100'},
+            {email: 'ada@lovelace.com', importance: true, subject: 'Programming', content: 'Enchantress of Numbers'},
+            {email: 'john@carmack.com', importance: false, subject: 'Updated Algo', content: 'New Algo for graphics stuff'},
+            {email: 'gabe@newell.com', importance: false, subject: 'HL3', content: 'Just Kidding!'}
+];
+
 }
